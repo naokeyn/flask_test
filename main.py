75ba0f6b60b4b32821d7e208d9883f70cfcd30bf
naokeyn <naoney-1228@outlook.jp>
2022-11-09 22:46:29 +0900

    change func and style

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from sqlalchemy import create_engine
 
 import pandas as pd
@@ -12,18 +12,6 @@ data_base = pd.read_sql("select * from df", engine)
 # 講義名のリスト
 names = data_base["subject_name"].tolist()
 
-# 部分一致のインデックスを返す
-# 同じ文字列にも対応させる
-def return_index(query):
-    global names
-    
-    index = []
-    for i in range(len(names)):
-        if query in names[i]:
-            index.append(i)
-            
-    return index    
-
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -33,37 +21,50 @@ def page_not_found(error):
 def Hello():
     return render_template("index.html")
    
-@app.route("/result", methods=["POST"])
+@app.route("/result", methods=["POST", "GET"])
 def result():
-    # フォームに入力された文字列を取得
-    query = request.form["name"]
-    
-    # 部分一致のindexを調べる
-    index = return_index(query)
-
-    # 一致する講義名がない場合
-    if len(index) == 0:
-        return render_template("page_not_found.html")
+    if request.method == "GET":
+        return redirect("/")
     
     else:
-        data = {
-            "name": data_base.loc[index, "subject_name"].tolist(),
-            "time": data_base.loc[index, "day_period"].tolist(),
-            "teac": data_base.loc[index, "teacher"].tolist(),
-            "room": data_base.loc[index, "room"].tolist(),
-            "length": len(index)
-        }
+        # フォームに入力された文字列を取得
+        query = request.form["name"]
         
-        return render_template("result.html", data=data)
+        # 部分一致のindexを調べる
+        # index = return_index(query)
+        
+        index = [i for i in range(len(data_base)) if query in data_base.loc[i, "subject_name"]]
+
+        # 一致する講義名がない場合
+        if len(index) == 0:
+            return render_template("page_not_found.html")
+        
+        else:
+            df = data_base.loc[index, :]
+            df = df.sort_values("room", ascending=False)
+            
+            data = {
+                "name": df["subject_name"].tolist(),
+                "time": df[ "day_period"].tolist(),
+                "teac": df["teacher"].tolist(),
+                "room": df["room"].tolist(),
+                "length": len(index)
+            }
+            
+            return render_template("result.html", data=data)
     
 @app.route("/all", methods=["GET", "POST"])
 def show_all():
-    lec_list = data_base["subject_name"].tolist()
-    per_list = data_base["day_period"].tolist()
-    room_list = data_base["room"].tolist()
-    length = len(lec_list)
+    df = data_base.sort_values("room", ascending=False)
     
-    data = {"lec":lec_list, "period":per_list, "room":room_list, "length":length}
+    data = {
+        "lec" : df["subject_name"].tolist(),
+        "period" : df["day_period"].tolist(),
+        "room" : df["room"].tolist(),
+        "length" : len(df)
+    }
+    
+    # data = {"lec":lec_list, "period":per_list, "room":room_list, "length":length}
     
     return render_template("show_all.html", data=data)
     
